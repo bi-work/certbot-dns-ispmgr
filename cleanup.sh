4161429dab9a4efe5134c7e11c83fc24893cdefe
Alex <ZAZmaster@users.noreply.github.com>
2018-10-18 12:18:03 +0500

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,11 +4,7 @@ _dir="$(dirname "$0")"
 
 source "$_dir/config.sh"
 
-# Strip only the top domain to get the zone id
-DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
-
 # Remove the challenge TXT record from the zone
-DOMAIN_ID="_acme-challenge.$DOMAIN. TXT  $CERTBOT_VALIDATION"
+DOMAIN_ID="_acme-challenge.$CERTBOT_DOMAIN. TXT  $CERTBOT_VALIDATION"
 RESULT=$(curl -s -X POST "https://$DNSMGR:1500/dnsmgr" \
-     -d "authinfo=$USER:$PASS&out=sjson&func=domain.record.delete&elid=$DOMAIN_ID&plid=$DOMAIN")
-echo $RESULT
+     -d "authinfo=$USER:$PASS&out=sjson&func=domain.record.delete&elid=$DOMAIN_ID&plid=$CERTBOT_DOMAIN")
