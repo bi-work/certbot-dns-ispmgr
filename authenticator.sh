905b15b4c51dd8bb07be3174cfda93477e7d83ed
Alex <ZAZmaster@users.noreply.github.com>
2018-10-18 11:53:18 +0500

    Update authenticator.sh

--- authenticator.sh
+++ authenticator.sh
@@ -8,9 +8,9 @@ source "$_dir/config.sh"
 DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
 	
 # Create TXT record
-CREATE_DOMAIN="_acme-challenge.$CERTBOT_DOMAIN"
+CREATE_DOMAIN="_acme-challenge"
 RECORD_ID=$(curl -s -X POST "https://$DNSMGR:1500/dnsmgr" \
-     -d "authinfo=$USER:$PASS&out=sjson&func=domain.record.edit&elid=&plid=$CERTBOT_DOMAIN&name=$CERTBOT_DOMAIN&ttl=3600&rtype=txt&value=$CERTBOT_VALIDATION&sok=ok")
+     -d "authinfo=$USER:$PASS&out=sjson&func=domain.record.edit&elid=&plid=$DOMAIN&name=$CREATE_DOMAIN&ttl=3600&rtype=txt&value=$CERTBOT_VALIDATION&sok=ok")
 
 # Sleep to make sure the change has time to propagate over to DNS
 sleep 5
