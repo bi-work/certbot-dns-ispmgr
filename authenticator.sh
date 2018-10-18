705697ced7c4b873cee150d46928b30f99033bb2
Alex <ZAZmaster@users.noreply.github.com>
2018-10-18 11:14:10 +0500

    Move from Yandex to DNSmanager

--- authenticator.sh
+++ authenticator.sh
@@ -9,17 +9,8 @@ DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
 	
 # Create TXT record
 CREATE_DOMAIN="_acme-challenge.$CERTBOT_DOMAIN"
-RECORD_ID=$(curl -s -X POST "https://pddimp.yandex.ru/api2/admin/dns/add" \
-     -H "PddToken: $API_KEY" \
-     -d "domain=$CERTBOT_DOMAIN&type=TXT&content=$CERTBOT_VALIDATION&ttl=3600&subdomain=$CREATE_DOMAIN" \
-	 | python -c "import sys,json;print(json.load(sys.stdin)['record']['record_id'])")
-	
-# Save info for cleanup
-if [ ! -d /tmp/CERTBOT_$CERTBOT_DOMAIN ];then
-        mkdir -m 0700 /tmp/CERTBOT_$CERTBOT_DOMAIN
-fi
-
-echo $RECORD_ID > /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
+RECORD_ID=$(curl -s -X POST "https://$DNSMGR:1500/dnsmgr" \
+     -d "authinfo=$USER:$PASS&out=sjson&func=domain.record.edit&elid=&plid=$CERTBOT_DOMAIN&name=$CERTBOT_DOMAIN&ttl=3600&rtype=txt&value=$CERTBOT_VALIDATION&sok=ok")
 
 # Sleep to make sure the change has time to propagate over to DNS
 sleep 5
