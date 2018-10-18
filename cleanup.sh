861394aaf00d6f8d48546a4facd573bd116a2dc6
Alex <ZAZmaster@users.noreply.github.com>
2018-10-18 11:14:18 +0500

    Move from Yandex to DNSmanager

--- cleanup.sh
+++ cleanup.sh
@@ -4,18 +4,8 @@ _dir="$(dirname "$0")"
 
 source "$_dir/config.sh"
 
-if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID ]; then
-        RECORD_ID=$(cat /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID)
-        rm -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
-fi
-
 # Remove the challenge TXT record from the zone
-if [ -n "${RECORD_ID}" ]; then
-	
-	RESULT=$(curl -s -X POST "https://pddimp.yandex.ru/api2/admin/dns/del" \
-     -H "PddToken: $API_KEY" \
-     -d "domain=$CERTBOT_DOMAIN&record_id=$RECORD_ID" \
-	 | python -c "import sys,json;print(json.load(sys.stdin)['success'])")
-	
-	echo $RESULT 
-fi
+CREATE_DOMAIN="_acme-challenge.$CERTBOT_DOMAIN"
+RESULT=$(curl -s -X POST "https://$DNSMGR:1500/dnsmgr" \
+     -d "authinfo=$USER:$PASS&out=sjson&func=domain.record.delete&elid=$CERTBOT_DOMAIN%20TXT%20%20$CERTBOT_VALIDATION&plid=$CERTBOT_DOMAIN")
+echo $RESULT
