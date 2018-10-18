883a473c9a9b442f076e9bd11a9336ae88a20ca2
Alex <ZAZmaster@users.noreply.github.com>
2018-10-18 16:40:28 +0500

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,3 +8,4 @@ source "$_dir/config.sh"
 DOMAIN_ID="_acme-challenge.$CERTBOT_DOMAIN. TXT  $CERTBOT_VALIDATION"
 RESULT=$(curl -s -X POST "https://$DNSMGR:1500/dnsmgr" \
      -d "authinfo=$USER:$PASS&out=sjson&func=domain.record.delete&elid=$DOMAIN_ID&plid=$CERTBOT_DOMAIN")
+echo $RESULT
