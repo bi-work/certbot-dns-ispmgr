72cbf9d886c66cefb74db5ef4561ce4d76ed668d
Sergey Balasov <sbalasov@gmail.com>
2020-05-13 10:36:59 +0700

    Allows insecure SSL connections

--- authenticator.sh
+++ authenticator.sh
@@ -6,7 +6,7 @@ source "$_dir/config.sh"
 
 # Create TXT record
 CREATE_DOMAIN="_acme-challenge"
-RESULT=$(curl -s -X POST "https://$ISPMGR:1500/ispmgr" \
+RESULT=$(curl -k -s -X POST "https://$ISPMGR:1500/ispmgr" \
   -d "authinfo=$USER:$PASS&out=sjson&func=domain.record.edit&elid=&plid=$CERTBOT_DOMAIN&name=$CREATE_DOMAIN&ttl=3600&rtype=txt&value=$CERTBOT_VALIDATION&sok=ok")
 
 # Sleep to make sure the change has time to propagate over to DNS
